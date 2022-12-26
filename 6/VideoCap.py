19fe26a8957138611899a39c79cd9326a4f5c892
RubyRode <dimaskrut71@gmail.com>
2022-12-27 03:43:08 +0700

    made some tries

--- 6/VideoCap.py
+++ 6/VideoCap.py
@@ -1,79 +1,94 @@
-import pandas as pd
 import numpy as np
 import cv2
 
+
+def switch(gm):
+    """for switching the game state"""
+    return not gm
+
+
 vid = cv2.VideoCapture(0)
-cv2.namedWindow("SETTINGS")
-cv2.createTrackbar('fil_par', "SETTINGS", 50, 255, lambda x: x)
-cv2.createTrackbar('matrix_size', "SETTINGS", 1, 25, lambda x: x)
-cv2.createTrackbar('iters', "SETTINGS", 7, 10, lambda x: x)
-cv2.createTrackbar('accx', "SETTINGS", 700, 800, lambda x: x)
-cv2.createTrackbar('accy', "SETTINGS", 700, 800, lambda x: x)
-cv2.createTrackbar('pre_dilate', "SETTINGS", 0, 30, lambda x: x)
-cv2.createTrackbar('thresh', "SETTINGS", 0, 255, lambda x: x)
+cv2.namedWindow("RESULT")
+cv2.namedWindow("RESULT2")
+cv2.createTrackbar('thresh', "RESULT", 15, 255, lambda x: x)
+cv2.createTrackbar('canny_x', "RESULT", 20, 255, lambda x: x)
+cv2.createTrackbar('canny_y', "RESULT", 20, 255, lambda x: x)
+cv2.createTrackbar('area_sta', "RESULT2", 100, 1000, lambda x: x)
+cv2.createTrackbar('area_mov', "RESULT2", 100, 1000, lambda x: x)
+g_b_m_size = 21
+# read first frame and prepare for future changes
+ret, prev_frame = vid.read()
+prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+prev_frame = cv2.GaussianBlur(prev_frame, (g_b_m_size, g_b_m_size), 0)
 
-prev_frame = None
-ret = True
+game_state = False
 while ret:
-
+    # read current frame and prepare it for future changes
     ret, cur_frame = vid.read()
+    orig = cur_frame.copy()
+    cur_frame = cv2.cvtColor(cur_frame, cv2.COLOR_BGR2GRAY)
+    cur_frame = cv2.GaussianBlur(cur_frame, (g_b_m_size, g_b_m_size), 0)
 
-    if not ret:
-        break
+    # find the difference between two frames and threshold them => motion capture mask
+    diff = cv2.absdiff(cur_frame, prev_frame)
+    threshold = cv2.getTrackbarPos('thresh', "RESULT")
+    thresh = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
 
-    # Filter an image
-    par = cv2.getTrackbarPos('fil_par', "SETTINGS")
-    cont_kernel = np.array([
-        [0, -1, 0],
-        [-1, par / 10, -1],
-        [0, -1, 0]
-    ])
-    filtered_cur = cv2.filter2D(cur_frame, -1, cont_kernel)
+    # find the mask for boundaries of the static objects => static objects mask
+    canny_x = cv2.getTrackbarPos('canny_x', "RESULT")
+    canny_y = cv2.getTrackbarPos('canny_y', "RESULT")
+    static = cv2.Canny(cur_frame.copy(), canny_x, canny_y)
 
-    # convert frame to gray colors
-    cur_gray = cv2.cvtColor(filtered_cur, cv2.COLOR_RGB2GRAY)
+    # dilate the masks
+    mov_erode = cv2.erode(thresh, None, 2)
+    mov_dilate = cv2.dilate(mov_erode, np.ones((3, 3), 'uint8'), 3)
+    sta_dilate = cv2.dilate(static, np.ones((3, 3), 'uint8'), 2)
 
+    if game_state:
+        # find contours from static objects mask
+        cnts_sta, _ = cv2.findContours(sta_dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+        area_sta = cv2.getTrackbarPos('area_sta', "RESULT2")
 
-    # blur frame
-    cur_blurred = cv2.GaussianBlur(cur_gray, (21, 21), 0)
-    # cur_blurred = cv2.blur(cur_blurred, (5, 5))
-    if prev_frame is None:
-        prev_frame = cur_blurred
-        prev_fr_blurred = cur_blurred.copy()
-    else:
-        pass
+        # find contours with the perimeter more than area_sta
+        cont_size_bool = []
+        for i in range(0, len(cnts_sta)):
+            if cv2.arcLength(cnts_sta[i], True) > area_sta:
+                cont_size_bool.append(cnts_sta[i])
 
-    diff1 = cv2.absdiff(cur_blurred, prev_fr_blurred)
-    tmp = cv2.getTrackbarPos('thresh', "SETTINGS")
-    thresh1 = cv2.threshold(diff1, tmp, 255, cv2.THRESH_BINARY)[1]
-    dil1 = cv2.dilate(thresh1.copy(), np.ones((5, 5), 'uint8'), 3)
+        # merging all found contours into one big contour
+        if len(cont_size_bool) != 0:
+            res_cont = np.vstack(cont_size_bool)
+            cv2.drawContours(orig, res_cont, -1, (0, 255, 0), 2)
 
-    accx = cv2.getTrackbarPos('accx', "SETTINGS")
-    accy = cv2.getTrackbarPos('accy', "SETTINGS")
-    grounds = cv2.Canny(cur_blurred.copy(), accx, accy, apertureSize=5)
+        # same process as above but for motion capture mask
+        cnts_mov, _ = cv2.findContours(mov_dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    pre_dilate = cv2.getTrackbarPos("pre_dilate", "SETTINGS")
+        area_mov = cv2.getTrackbarPos('area_mov', "RESULT2")
 
-    dilated = cv2.dilate(grounds.copy(), np.ones((pre_dilate + 10, pre_dilate + 10), 'uint8'), 7)
-    prev_dilated = cv2.dilate(prev_frame.copy(), np.ones((pre_dilate, pre_dilate), 'uint8'), 7)
+        cont_size_bool = []
+        for i in range(0, len(cnts_mov)):
+            if cv2.arcLength(cnts_mov[i], True) > area_mov:
+                cont_size_bool.append(cnts_mov[i])
+        if len(cont_size_bool) != 0:
+            res_cont = np.vstack(cont_size_bool)
+            cv2.drawContours(orig, res_cont, -1, (0, 0, 255), 2)
 
-    diff = cv2.absdiff(prev_dilated, dilated)
-    mx_s = cv2.getTrackbarPos('matrix_size', "SETTINGS")
-    iter = cv2.getTrackbarPos('iterations', "SETTINGS")
-    th_dil = cv2.dilate(diff, np.ones((mx_s, mx_s), 'uint8'), iter)
-    invert = cv2.bitwise_or(th_dil, dil1)
+    # merging motion capture and static object masks
+    res = np.hstack((sta_dilate, mov_dilate))
 
-    cv2.imshow("diff", invert)
+    # change frames
+    prev_frame = cur_frame
 
-    # find diff frame
-    # diff = cv2.absdiff(prev_frame, cur_blurred)
+    # show the results: two masks and the original video
+    cv2.imshow("RESULT", res)
+    cv2.imshow("RESULT2", orig)
 
-    prev_frame = dilated
-    prev_fr_blurred = cur_blurred
     key = cv2.waitKey(1)
     if key & 0xFF == ord("q"):
         break
+    if key & 0xFF == ord("n"):
+        game_state = switch(game_state)
 
 vid.release()
 cv2.destroyAllWindows()
