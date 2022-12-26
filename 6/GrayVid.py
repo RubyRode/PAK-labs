3c7b20284fc86949d7c4e7f0ee2cb8b0be9c2005
RubyRode <dimaskrut71@gmail.com>
2022-12-27 02:27:33 +0700

    made some tries

--- 6/GrayVid.py
+++ 6/GrayVid.py
@@ -1,15 +1,22 @@
 import cv2
+import numpy as np
 
-
-cap = cv2.VideoCapture("video_2022-12-06_06-07-29.mp4")
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error opening Video File.")
 try:
     while True:
         ret, frame = cap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         key = cv2.waitKey(20) & 0xff
-        cv2.imshow('Frame', frame)
+
+        # image_out = np.zeros_like(frame)
+        image_out = frame.copy()
+        for i in range(3):
+            image_out[:, :, i] = frame[:, :, 1]
+
+        cv2.imshow('Frame', image_out)
+
         if key == 27 | (not ret):
             cv2.destroyAllWindows()
             cap.release()
--- 6/VideoCap.py
+++ 6/VideoCap.py
@@ -2,68 +2,75 @@ import pandas as pd
 import numpy as np
 import cv2
 
-vid = cv2.VideoCapture(0)
-ret, prev_frame = vid.read()
-prev_frame1 = prev_frame.copy()
-# prepare the first frame
-prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_RGB2GRAY)
-prev_blured = cv2.GaussianBlur(prev_gray, (21, 21), 0)
+vid = cv2.VideoCapture("test3.mp4")
 cv2.namedWindow("SETTINGS")
-cv2.createTrackbar('thresh_sta', "SETTINGS", 0, 255, lambda x: x)
-cv2.createTrackbar('thresh_mov', "SETTINGS", 0, 255, lambda x: x)
-# cv2.createTrackbar('fil_par', "SETTINGS", 0, 255, lambda x: x)
-cv2.createTrackbar('matrix_size', "SETTINGS", 0, 25, lambda x: x)
-cv2.createTrackbar('iters', "SETTINGS", 0, 10, lambda x: x)
+cv2.createTrackbar('thresh', "SETTINGS", 0, 255, lambda x: x)
+cv2.createTrackbar('area_size', "SETTINGS", 50, 10000, lambda x: x)
+cv2.createTrackbar('fil_par', "SETTINGS", 50, 255, lambda x: x)
+cv2.createTrackbar('matrix_size', "SETTINGS", 1, 25, lambda x: x)
+cv2.createTrackbar('iters', "SETTINGS", 7, 10, lambda x: x)
+cv2.createTrackbar('accx', "SETTINGS", 700, 800, lambda x: x)
+cv2.createTrackbar('accy', "SETTINGS", 700, 800, lambda x: x)
+# cv2.createTrackbar('apper', "SETTINGS", 3, 6, lambda x: x if x % 2 == 1 else x + 1)
 
+prev_frame = None
+ret = True
 while ret:
+
     ret, cur_frame = vid.read()
+
     if not ret:
         break
 
-    # par = cv2.getTrackbarPos('fil_par', "SETTINGS")
+    # Filter an image
+    par = cv2.getTrackbarPos('fil_par', "SETTINGS")
     cont_kernel = np.array([
         [0, -1, 0],
-        [-1, 5, -1],
+        [-1, par / 10, -1],
         [0, -1, 0]
     ])
-
-    filtered_cur = cv2.filter2D(cur_frame, -1,  cont_kernel)
+    filtered_cur = cv2.filter2D(cur_frame, -1, cont_kernel)
 
     # convert frame to gray colors
     cur_gray = cv2.cvtColor(filtered_cur, cv2.COLOR_RGB2GRAY)
 
+    if prev_frame is None:
+        prev_frame = cur_gray
+    else:
+        pass
+
     # blur frame
     cur_blurred = cv2.GaussianBlur(cur_gray, (21, 21), 0)
+    cur_blurred = cv2.blur(cur_blurred, (5, 5))
+
+    accx = cv2.getTrackbarPos('accx', "SETTINGS")
+    accy = cv2.getTrackbarPos('accy', "SETTINGS")
+    # apper = cv2.getTrackbarPos('apper', "SETTINGS")
+    grounds = cv2.Canny(cur_blurred.copy(), accx, accy, apertureSize=5)
+    dilated = cv2.dilate(grounds.copy(), None, 2)
+    prev_dilated = cv2.dilate(prev_frame.copy(), None, 2)
+
+    diff = cv2.absdiff(dilated, prev_dilated)
+    thresh_ = cv2.getTrackbarPos('thresh', "SETTINGS")
+    thresh = cv2.threshold(diff.copy(), thresh_, 255, cv2.THRESH_BINARY)[1]
+    mx_s = cv2.getTrackbarPos('matrix_size', "SETTINGS")
+    iter = cv2.getTrackbarPos('iterations', "SETTINGS")
+    th_dil = cv2.dilate(thresh, np.ones((mx_s, mx_s), 'uint8'), iter)
+
+    cnts = cv2.findContours(th_dil, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+    area = cv2.getTrackbarPos('area_size', "SETTINGS")
+    for contour in cnts:
+        if cv2.contourArea(contour) > area:
+            cv2.drawContours(cur_frame, contour, 3, (0, 0, 255), 3)
+    cv2.imshow("diff", cur_frame)
 
     # find diff frame
-    diff = cv2.absdiff(prev_blured, cur_blurred)
-    # threshed frame
-    thresh_sta = cv2.getTrackbarPos('thresh_sta', "SETTINGS")
-    thresh_mov = cv2.getTrackbarPos('thresh_mov', "SETTINGS")
+    diff = cv2.absdiff(prev_frame, cur_blurred)
 
-    threshed_sta = cv2.threshold(diff, thresh_sta, 255, cv2.THRESH_BINARY)[1]
-    threshed_mov = cv2.threshold(diff, thresh_mov, 255, cv2.THRESH_BINARY)[1]
-    # dilated frame
-    dilated_sta = cv2.dilate(threshed_sta, None, iterations=2)
-    iters = cv2.getTrackbarPos('iters', "SETTINGS")
-    mat_size = cv2.getTrackbarPos('matrix_size', "SETTINGS")
-    dilated_mov = cv2.dilate(threshed_mov, np.ones((mat_size, mat_size), 'uint8'), iterations=iters)
-    # find contour
-    cnts_sta = cv2.findContours(dilated_sta, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    cnts_mov = cv2.findContours(dilated_mov, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    # draw contours
-    cv2.drawContours(cur_frame, cnts_sta, -1, (0, 255, 0), 3)
-    cv2.drawContours(cur_frame, cnts_mov, -1, (0, 0, 255), 3)
-
-    prev_blured = cur_blurred
-
-    cv2.imshow("cur", cur_frame)
-    cv2.imshow("filtered", filtered_cur)
-
-    key = cv2.waitKey(20)
+    prev_frame = grounds
+    key = cv2.waitKey(1)
     if key & 0xFF == ord("q"):
         break
-
 
 vid.release()
 cv2.destroyAllWindows()
