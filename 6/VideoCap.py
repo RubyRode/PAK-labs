e7d48be5a086dc64b6f9ca184c2fe8f74a5d5df3
RubyRode <dimaskrut71@gmail.com>
2022-12-27 16:48:10 +0700

    made some tries

--- 6/VideoCap.py
+++ 6/VideoCap.py
@@ -2,16 +2,15 @@ import pandas as pd
 import numpy as np
 import cv2
 
-vid = cv2.VideoCapture("test3.mp4")
+vid = cv2.VideoCapture(0)
 cv2.namedWindow("SETTINGS")
-cv2.createTrackbar('thresh', "SETTINGS", 0, 255, lambda x: x)
-cv2.createTrackbar('area_size', "SETTINGS", 50, 10000, lambda x: x)
 cv2.createTrackbar('fil_par', "SETTINGS", 50, 255, lambda x: x)
 cv2.createTrackbar('matrix_size', "SETTINGS", 1, 25, lambda x: x)
 cv2.createTrackbar('iters', "SETTINGS", 7, 10, lambda x: x)
 cv2.createTrackbar('accx', "SETTINGS", 700, 800, lambda x: x)
 cv2.createTrackbar('accy', "SETTINGS", 700, 800, lambda x: x)
-# cv2.createTrackbar('apper', "SETTINGS", 3, 6, lambda x: x if x % 2 == 1 else x + 1)
+cv2.createTrackbar('pre_dilate', "SETTINGS", 0, 30, lambda x: x)
+cv2.createTrackbar('thresh', "SETTINGS", 0, 255, lambda x: x)
 
 prev_frame = None
 ret = True
@@ -34,40 +33,44 @@ while ret:
     # convert frame to gray colors
     cur_gray = cv2.cvtColor(filtered_cur, cv2.COLOR_RGB2GRAY)
 
-    if prev_frame is None:
-        prev_frame = cur_gray
-    else:
-        pass
+
 
     # blur frame
     cur_blurred = cv2.GaussianBlur(cur_gray, (21, 21), 0)
-    cur_blurred = cv2.blur(cur_blurred, (5, 5))
+    # cur_blurred = cv2.blur(cur_blurred, (5, 5))
+    if prev_frame is None:
+        prev_frame = cur_blurred
+        prev_fr_blurred = cur_blurred.copy()
+    else:
+        pass
+
+    diff1 = cv2.absdiff(cur_blurred, prev_fr_blurred)
+    tmp = cv2.getTrackbarPos('thresh', "SETTINGS")
+    thresh1 = cv2.threshold(diff1, tmp, 255, cv2.THRESH_BINARY)[1]
+    dil1 = cv2.dilate(thresh1.copy(), np.ones((5, 5), 'uint8'), 3)
 
     accx = cv2.getTrackbarPos('accx', "SETTINGS")
     accy = cv2.getTrackbarPos('accy', "SETTINGS")
-    # apper = cv2.getTrackbarPos('apper', "SETTINGS")
     grounds = cv2.Canny(cur_blurred.copy(), accx, accy, apertureSize=5)
-    dilated = cv2.dilate(grounds.copy(), None, 2)
-    prev_dilated = cv2.dilate(prev_frame.copy(), None, 2)
 
-    diff = cv2.absdiff(dilated, prev_dilated)
-    thresh_ = cv2.getTrackbarPos('thresh', "SETTINGS")
-    thresh = cv2.threshold(diff.copy(), thresh_, 255, cv2.THRESH_BINARY)[1]
+    pre_dilate = cv2.getTrackbarPos("pre_dilate", "SETTINGS")
+
+    dilated = cv2.dilate(grounds.copy(), np.ones((pre_dilate + 10, pre_dilate + 10), 'uint8'), 7)
+    prev_dilated = cv2.dilate(prev_frame.copy(), np.ones((pre_dilate, pre_dilate), 'uint8'), 7)
+
+    diff = cv2.absdiff(prev_dilated, dilated)
     mx_s = cv2.getTrackbarPos('matrix_size', "SETTINGS")
     iter = cv2.getTrackbarPos('iterations', "SETTINGS")
-    th_dil = cv2.dilate(thresh, np.ones((mx_s, mx_s), 'uint8'), iter)
+    th_dil = cv2.dilate(diff, np.ones((mx_s, mx_s), 'uint8'), iter)
+    invert = cv2.bitwise_or(th_dil, dil1)
 
-    cnts = cv2.findContours(th_dil, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
-    area = cv2.getTrackbarPos('area_size', "SETTINGS")
-    for contour in cnts:
-        if cv2.contourArea(contour) > area:
-            cv2.drawContours(cur_frame, contour, 3, (0, 0, 255), 3)
-    cv2.imshow("diff", cur_frame)
+    cv2.imshow("diff", invert)
 
     # find diff frame
-    diff = cv2.absdiff(prev_frame, cur_blurred)
+    # diff = cv2.absdiff(prev_frame, cur_blurred)
 
-    prev_frame = grounds
+    prev_frame = dilated
+    prev_fr_blurred = cur_blurred
     key = cv2.waitKey(1)
     if key & 0xFF == ord("q"):
         break
