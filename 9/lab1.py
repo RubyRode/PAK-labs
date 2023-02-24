a035e54baf66be7cb5fe6ae893fb60384d9487bf
RubyRode <dimaskrut71@gmail.com>
2023-02-25 03:03:42 +0700

    3 pretrained models + accuracy

--- 9/lab1.py
+++ 9/lab1.py
@@ -1,14 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import xgboost
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from param_grids import grids
 
-data_labels = pd.read_csv('/home/ruby/PycharmProjects/PAK-labs/titanic/titanic_prepared.csv')
-data = data_labels.drop('Unnamed: 0', axis=1)
+data = pd.read_csv('/home/ruby/PycharmProjects/PAK-labs/titanic/titanic_prepared.csv')
+data = data.drop('Unnamed: 0', axis=1)
 labels = data['label']
 data = data.drop('label', axis=1)
 
@@ -29,15 +28,14 @@ xgboost_model = xgboost.XGBClassifier(**grids['xgboost_best'])
 xgboost_model.fit(test_x, test_y)
 print('XGBoost accuracy: ', xgboost_model.score(test_x, test_y))
 
-# Logistic Regression 2 parameters
+# Decision tree 2 parameters
 num = 2
 importances = decision_tree_model.feature_importances_
 indices = np.argsort(importances)
-features = train_x.columns[indices[(len(indices) - 1 - num)]: len(indices) - 1]
-indices = indices[(len(indices) - 2 - num): len(indices) - 1]
-decision_tree_model.fit(train_x[features], train_y)
-print(features)
-print(indices)
-print(train_x.columns)
-predict = decision_tree_model.predict(train_x[features])
-print("accuracy: ", decision_tree_model.score(train_y, predict))
+features = train_x.columns
+indices = indices[(len(indices) - 1 - num): len(indices) - 1]
+test_x = test_x[features[indices]]
+decision_tree_model.fit(test_x, test_y)
+
+print("Accuracy by 2 parameters: ", decision_tree_model.score(test_x, test_y))
+
