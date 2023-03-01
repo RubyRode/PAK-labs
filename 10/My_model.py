533b1d0432d9e748b9f9512bf45553d5c7074666
RubyRode <dimaskrut71@gmail.com>
2023-03-02 02:09:18 +0700

    Improved forward propagation

--- 10/My_model.py
+++ 10/My_model.py
@@ -1,47 +1,46 @@
 import numpy as np
-from numpy import ndarray
+
+
+def softmax(x):
+    """Softmax function for my model"""
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+def relu(x):
+    """Relu function for my model"""
+    return np.maximum(0, x)
+
+
+def tanh(x):
+    """Hyperbolic tangent function for my model"""
+    return np.tanh(x)
 
 
 class Model:
 
-    def __init__(self, X: ndarray):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size):
         """Model initialization"""
-        self.X = X
-        self.outs = [64, 16, 4]
-        self.shape = self.X.shape
-        self.kernel = None
-        self.act = {'relu': self.relu, 'softmax': self.softmax, 'tanh': self.tanh}
+        self.weights_1 = np.random.randn(input_size)
+        self.bias_1 = np.random.randn(hidden_size_1)
+        self.weights_2 = np.random.randn(hidden_size_1)
+        self.bias_2 = np.random.randn(hidden_size_2)
+        self.weights_3 = np.random.randn(hidden_size_2)
+        self.bias_3 = np.random.randn(output_size)
+        self.act = {'soft': softmax, 'tanh': tanh, 'relu': relu}
 
-    def forward(self, activation='relu'):
-        """Parameters convolution"""
-
-        for j in self.outs:
-            self.kernel = np.random.rand(self.X.shape[0] - j + 1)
-            while self.X.shape[0] > j:
-                self.X = np.convolve(self.X, self.kernel, 'valid')
-                self.shape = self.X.shape
-            self.act[activation]()
-            print(self.X, "\n", self.X.shape, '\n')
-
-    def relu(self):
-        """Relu function for my model"""
-        for j in range(0, self.X.shape[0]):
-            self.X[j] = max(0, self.X[j])
-
-    def softmax(self):
-        """Softmax function for my model"""
-        e_x = np.exp(self.X - np.max(self.X))
-        self.X = e_x / e_x.sum()
-
-    def tanh(self):
-        """Hyperbolic tangent function for my model"""
-        self.X = np.tanh(self.X)
+    def forward(self, input_data, activation='relu'):
+        """Forward propagation"""
+        # np.squeeze(input_data)
+        layer_1 = self.act[activation](np.dot(self.weights_1, input_data) + self.bias_1)
+        layer_2 = self.act[activation](np.dot(self.weights_2, layer_1) + self.bias_2)
+        layer_3 = self.act[activation](np.dot(self.weights_3, layer_2) + self.bias_3)
+        return layer_3
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     data = np.random.randn(256)
-    print(data)
-    some_model = Model(data)
-    some_model.forward('softmax')
-    print(sum(some_model.X))
+    in_outs = [256, 64, 16, 4]
+    model = Model(*in_outs)
+    res = model.forward(data, 'soft')
+    print(res, '\n', sum(res))
